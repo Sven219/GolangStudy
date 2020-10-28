504eeb73551222c9c278c998b5955ac18c46c56b
sven <swq1227@gmail.com>
2020-10-28 14:58:18 +0800

    文件操作

---
+++ Go语言入门经典/hour21/writeFileContent.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	s := "hello world!"
+	err := ioutil.WriteFile("file03.txt", []byte(s), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
