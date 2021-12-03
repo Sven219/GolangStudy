c9349f15b6a572065c2cda9330a7430bf3c9c25e
sven <swq1227@gmail.com>
2021-12-03 17:22:54 +0800

    add 99乘法表

--- oldBoy/day01/helloworld/homework.go
+++ oldBoy/day01/helloworld/homework.go
@@ -29,6 +29,14 @@ func main() {
 			count++
 		}
 	}
-	fmt.Print(count)
+	fmt.Println(count)
+
+	fmt.Println("九九乘法表")
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%dx%d=%d ", i, j, i*j)
+		}
+		fmt.Println()
+	}
 
 }
