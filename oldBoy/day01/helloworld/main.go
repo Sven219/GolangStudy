ebe6d1efbb829945427ee554cff03d4f6ab22d87
sven <swq1227@gmail.com>
2021-12-03 16:34:34 +0800

    add 类型

--- oldBoy/day01/helloworld/main.go
+++ oldBoy/day01/helloworld/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const pi = 3.14
+const (
+	a1 = iota
+	a2
+	a3
+	a4
+)
 
 func main() {
 	fmt.Println("hello world")
+	data := "test"
+	fmt.Printf("%s", data)
+	fmt.Println()
+	fmt.Print(pi)
+	fmt.Println("aa1:", a1)
+	fmt.Println("aa2", a2)
+	// 修改字符串
+	s1 := "白萝卜"
+	s2 := []rune(s1)
+	s2[0] = '红'
+	fmt.Println(string(s2))
 }
