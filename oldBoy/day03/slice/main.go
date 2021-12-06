afd2c3c0f95e3d506a060db0444d6a73b1bab3ef
sven <swq1227@gmail.com>
2021-12-06 13:59:36 +0800

    update slice

--- oldBoy/day03/slice/main.go
+++ oldBoy/day03/slice/main.go
@@ -20,6 +20,11 @@ func main() {
 	// 长度和容量
 	fmt.Printf("len:%d cap:%d\n", len(s1), cap(s1))
 	fmt.Printf("len:%d cap:%d\n", len(s2), cap(s2))
+	// append 时容量翻倍
+	s1 = append(s1, 3)
+	s1 = append(s1, 4, 5, 7)
+	fmt.Println(s1)
+	fmt.Printf("len:%d cap:%d\n", len(s1), cap(s1))
 
 	// 数组转切片
 	a1 := [...]int{1, 3, 5, 7, 9}
