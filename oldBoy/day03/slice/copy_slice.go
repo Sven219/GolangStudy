957cf7c36ad24916c787dcc1b11bd823b5a14b08
sven <swq1227@gmail.com>
2021-12-06 15:26:44 +0800

    add copy and delete

--- oldBoy/day03/slice/copy_slice.go
+++ oldBoy/day03/slice/copy_slice.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 拷贝
+	a1 := []int{1, 2, 3}
+	a2 := a1
+	a3 := make([]int, len(a1))
+	copy(a3, a1)
+	fmt.Println(a1, a2, a3) // [1 2 3] [1 2 3] [1 2 3]
+	a1[0] = 4
+	fmt.Println(a1, a2, a3) // [4 2 3] [4 2 3] [1 2 3]
+
+	// 删除元素
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// 删除索引为 2 的元素
+	a = append(a[:2], a[3:]...)
+	fmt.Println(a)
 }
