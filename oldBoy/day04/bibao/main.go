e07ebc1b821bcdf0061dbaf1432774b08439f31d
sven <swq1227@gmail.com>
2021-12-08 16:09:49 +0800

    add 闭包

--- oldBoy/day04/bibao/main.go
+++ oldBoy/day04/bibao/main.go
@@ -1,6 +1,26 @@
 package main
 
-// 闭包
-func main() {
+import "fmt"
 
+// 闭包
+
+// func f1(f func()) {
+// 	fmt.Println("This is f1")
+
+// }
+func f2(x, y int) {
+	fmt.Println("This is f2")
+	fmt.Println(x + y)
+}
+
+// 定义一个函数对 f2 进行包装
+func f3(f func(int, int), m int, n int) func() {
+	tmp := func() {
+		f(m, n)
+	}
+	return tmp
+}
+func main() {
+	ret := f3(f2, 100, 200)
+	ret()
 }
