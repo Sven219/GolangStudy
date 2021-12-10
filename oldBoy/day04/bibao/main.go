3aa3a5470b7f8b8d24c57b106d8cc09291d75eb3
sven <swq1227@gmail.com>
2021-12-10 14:07:10 +0800

    add 闭包

--- oldBoy/day04/bibao/main.go
+++ oldBoy/day04/bibao/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 // 闭包
-
-// func f1(f func()) {
-// 	fmt.Println("This is f1")
+// 闭包是一个函数，这个函数能包含它外部作用域的一个变量，
+// 底层原理
+// 1. 函数可以作为返回值
+// 2. 函数内部查找变量的顺序： 现先在自己内部找，找不到往外层找
 
 // }
 func f2(x, y int) {
