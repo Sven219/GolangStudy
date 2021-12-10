e6fc3671d9cabb6a1e46094556a5b7c4ef2ebc0e
sven <swq1227@gmail.com>
2021-12-10 15:07:31 +0800

    update struct

--- oldBoy/day05/struct/main.go
+++ oldBoy/day05/struct/main.go
@@ -15,7 +15,17 @@ func main() {
 	lihua.age = 19
 	lihua.hobby = []string{"吃", "喝"}
 	lihua.name = "李华"
+	fmt.Printf("%T\n", lihua)
+	fmt.Println(lihua)
 
-	fmt.Println(lihua.age)
+	// 匿名结构体
+	var s struct {
+		name string
+		age  int
+	}
+	s.age = 1
+	s.name = "张三"
 
+	fmt.Printf("%T\n", s) //struct { name string; age int }
+	fmt.Println(s) // {张三 1}
 }
