65264f4d4c5a62da179dfe0ccc352206d5810c17
sven <swq1227@gmail.com>
2021-12-12 14:21:29 +0800

    add Method

---
+++ oldBoy/day06/struct/method.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type dog struct {
+	name string
+}
+
+// 构造函数
+func newDog(name string) dog {
+	return dog{
+		name: name,
+	}
+}
+
+// 方法是作用于特定类型的函数
+func (d dog) wang() {
+	fmt.Println("旺旺旺~")
+}
+
+func main() {
+	d1 := newDog("旺财")
+	d1.wang()
+}
