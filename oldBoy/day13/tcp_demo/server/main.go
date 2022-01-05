9ad2c043811a23ee5661b4c533f9e8e929240b60
sven <swq1227@gmail.com>
2022-01-05 11:34:07 +0800

    update tcp server and client

--- oldBoy/day13/tcp_demo/server/main.go
+++ oldBoy/day13/tcp_demo/server/main.go
@@ -7,25 +7,38 @@ import (
 
 // tcp server
 
+func processConn(conn net.Conn) {
+	// 3. 与客户端通信
+	var tmp [128]byte
+
+	for {
+		n, err := conn.Read(tmp[:])
+		if err != nil {
+			fmt.Println("read from conn failed ,err", err)
+			return
+		}
+		fmt.Println(string(tmp[:n]))
+	}
+
+}
+
 func main() {
 	// 1.  本地端口启动服务
 	listener, err := net.Listen("tcp", "127.0.0.1:20000")
+	fmt.Println("Wait...")
 	if err != nil {
 		fmt.Println("Start server on 127.0.0.1:20000 failed：", err)
 		return
 	}
 	// 1. 等待别人来链接
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("accept failed ,err :", err)
-		return
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("accept failed ,err :", err)
+			return
+		}
+		go processConn(conn)
+
 	}
-	// 3. 与客户端通信
-	var tmp [128]byte
-	n, err := conn.Read(tmp[:])
-	if err != nil {
-		fmt.Println("read from conn failed ,err", err)
-		return
-	}
-	fmt.Println(string(tmp[:n]))
+
 }
