e406401ac3c3f65044beba80c3605c3441caaf9d
sven <swq1227@gmail.com>
2022-02-22 16:12:06 +0800

    sql query

--- oldBoy/day15/mysql/main.go
+++ oldBoy/day15/mysql/main.go
@@ -7,11 +7,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+var db *sql.DB // 是一个连接池对象
+
+func initDB() (err error) {
 	//数据库信息
 	dsn := "root:root@tcp(10.158.1.3:3306)/sven_test"
 	// 连接数据库
-	db, err := sql.Open("mysql", dsn) // 不会校验用户名和密码是否正确
+	db, err = sql.Open("mysql", dsn) // 不会校验用户名和密码是否正确
 	if err != nil {
 		fmt.Printf("dsn: %s inbvalid failed,err:%v\n", dsn, err)
 		return
@@ -21,6 +23,37 @@ func main() {
 		fmt.Printf("open %s failed,err:%v\n", dsn, err)
 		return
 	}
-	db.Close()
+	// 设置数据库连接池最大连接数
+	db.SetMaxOpenConns(10)
+	return
+}
+
+type user struct {
+	id   int
+	name string
+	age  int
+}
+
+// 查询
+func query() {
+
+}
+
+func insert() {}
+
+func main() {
+	err := initDB()
+	if err != nil {
+		fmt.Printf("init DB failed ,err :%v\n", err)
+	}
 	fmt.Println("successful！")
+	var u1 user
+	// 1. 写查询单条记录的 sql 语句
+	sqlStr := "select id,name,age from user where id =?;"
+	// 2. 执行
+	rowObj := db.QueryRow(sqlStr, 1) // 从连接池里拿一个连接去数据库查询单条记录
+	rowObj.Scan(&u1.id, &u1.name, &u1.age)
+	// 打印结果
+	fmt.Printf("u1:%#v\n", u1)
+
 }
